04a5a2336481ec8d421bb3180cf057a227094d65
Madhan Kumar B S <MadhanBhonsle.Somu@ihg.com>
2019-12-06 14:29:31 -0500

    Updates to File path

--- src/test/java/resources/PayloadConfigs.java
+++ src/test/java/resources/PayloadConfigs.java
@@ -3,9 +3,9 @@ package resources;
 public class PayloadConfigs {
 	
 	
-public static String 	TGT_NORM_FP="src/test/resources/createres/op/createSomething.json";
-public static String	NORM_FP="src/test/resources/createres/createSomething.json";
-public static String	OTH_PATH="src/test/resources/createres/CreateOtherThing.json";
-public static String	TGT_OTH_PATH="src/test/resources/createres/op/CreateOtherThing.json";
+public static String 	TGT_NORM_FP="src/test/resources/create/op/createSomething.json";
+public static String	NORM_FP="src/test/resources/create/createSomething.json";
+public static String	OTH_PATH="src/test/resources/create/CreateOtherThing.json";
+public static String	TGT_OTH_PATH="src/test/resources/create/op/CreateOtherThing.json";
 
 }
